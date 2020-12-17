d47500458b0ad55b7a36f085012b725dc3d533f6
YEJIKIM13 <lerry597@gmail.com>
2020-12-17 19:17:23 +0900

    받은 달의 최대일수 구하기 작성 답안

--- src/lerry/calendar/Calendar.java
+++ src/lerry/calendar/Calendar.java
@@ -1,4 +1,5 @@
 package lerry.calendar;
+import java.util.Scanner;
 
 public class Calendar {
 
@@ -10,6 +11,21 @@ public class Calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		
+		System.out.println("달을 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		if (A == 2) {
+			System.out.println("2월은 28일까지 있습니다.");
+		} else if (A == 4 || A == 6 || A == 9 || A == 11) {
+			System.out.printf("%d월은 30일까지 있습니다", A);
+		} else {
+			System.out.printf("%d월은 31일까지 있습니다.", A);
+		}
+		scanner.close();
+		
 	}
 
-}
+} 
