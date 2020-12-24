8c769c71c79f8780087af310686964272942b5d9
YEJIKIM13 <lerry597@gmail.com>
2020-12-24 20:14:27 +0900

    가상의 달력 찍기(작성한 답안)

--- src/lerry/calendar/Calendar.java
+++ src/lerry/calendar/Calendar.java
@@ -8,13 +8,31 @@ import java.util.Scanner;
 		public int getMaxDaysOfMonth(int month) {
 			return maxDays[month - 1];
 		}
-		public void printSampleCalendar() {
+		public static void printSampleCalendar28() {
 			System.out.println("일 월 화 수 목 금 토");
 			System.out.println("--------------------");
 			System.out.println(" 1  2  3  4  5  6  7");
 			System.out.println(" 8  9 10 11 12 13 14");
 			System.out.println("15 16 17 18 19 20 21");
 			System.out.println("22 23 24 25 26 27 28");
+		}
+		public static void printSampleCalendar30() {
+			System.out.println("일 월 화 수 목 금 토");
+			System.out.println("--------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30");
+		}
+		public static void printSampleCalendar31() {
+			System.out.println("일 월 화 수 목 금 토");
+			System.out.println("--------------------");
+			System.out.println(" 1  2  3  4  5  6  7");
+			System.out.println(" 8  9 10 11 12 13 14");
+			System.out.println("15 16 17 18 19 20 21");
+			System.out.println("22 23 24 25 26 27 28");
+			System.out.println("29 30 31");
 		}
 	public static void main(String[] args){
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램 + 반복
@@ -33,9 +51,15 @@ import java.util.Scanner;
 			if(month > 12) {
 				continue;
 			}
-				System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+			if(month==1||month==3||month==5||month==7||month==8||month==10||month==12) {	
+				printSampleCalendar31();
+			} else if(month==2) {
+				printSampleCalendar28();
+			} else {
+				printSampleCalendar30();
 			}
-		System.out.println("Have a nice day!");
+		}
+		System.out.println("Bye~");
 		scanner.close();
 	}
 
